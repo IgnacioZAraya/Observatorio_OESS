1ad180e6c3b792df70ce897738f5d3ffcc1e32c8
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-04-22 19:36:31 -0600

    First Stage of the Header

--- observatorio_oess/src/pages/header/components/index.tsx
+++ observatorio_oess/src/pages/header/components/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
-import logoMTSS from '../../../assets/Image/MTSSAZUL.png';
+import logoMTSS from "../../../assets/Image/MTSSDORADO.png";
+import logoFB from "../../../assets/Image/RedesSoaciales/fb_icon.png";
+import logoIG from "../../../assets/Image/RedesSoaciales/Ig_Icon.png";
+import logoX from "../../../assets/Image/RedesSoaciales/X_Icon.png";
+import logoYT from "../../../assets/Image/RedesSoaciales/Yt_Icon.png";
+import logoLK from "../../../assets/Image/RedesSoaciales/Lk_Icon.png";
+import logoEM from "../../../assets/Image/RedesSoaciales/Em_Icon.png";
 import "../style.css";
-
 
 function Header() {
   return (
@@ -10,32 +15,8 @@ function Header() {
         <div className="header-top">
           {/* /* Logotipo y redes sociales */}
           <div className="logo-section">
-            <img
-              src={logoMTSS}
-              alt="Logo del Ministerio"
-              className="logo"
-            />
+            <img src={logoMTSS} alt="Logo del Ministerio" className="logo" />
             {/* /* Redes Sociales */}
-            <div className="social-icons">
-              <a href="#">
-                <i className="fab fa-facebook"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-instagram"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-twitter"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-youtube"></i>
-              </a>
-              <a href="#">
-                <i className="fab fa-linkedin"></i>
-              </a>
-              <a href="#">
-                <i className="fas fa-envelope"></i>
-              </a>
-            </div>
           </div>
           {/* <!-- Navegación superior y barra de búsqueda --> */}
           <div className="nav-top">
@@ -48,9 +29,43 @@ function Header() {
               <a href="#">Denuncia Auditoría</a>
             </div>
             {/* <!-- Barra de búsqueda --> */}
-            <div className="search-bar">
-              <input type="text" placeholder="Buscar"></input>
-              <button>Buscar</button>
+            <div className="row">
+              <div className="social-icons">
+                <img
+                  src={logoFB}
+                  alt="Logo del Ministerio"
+                  className="fab fa-facebook"
+                />
+                <img
+                  src={logoIG}
+                  alt="Logo del Ministerio"
+                  className="fab fa-instagram"
+                />
+                <img
+                  src={logoX}
+                  alt="Logo del Ministerio"
+                  className="fab fa-twitter"
+                />
+                <img
+                  src={logoYT}
+                  alt="Logo del Ministerio"
+                  className="fab fa-youtube"
+                />
+                <img
+                  src={logoLK}
+                  alt="Logo del Ministerio"
+                  className="fab fa-linkedin"
+                />
+                <img
+                  src={logoEM}
+                  alt="Logo del Ministerio"
+                  className="fas fa-envelope"
+                />
+              </div>
+              <div className="search-bar">
+                <input type="text" placeholder="Buscar"></input>
+                <button>Buscar</button>
+              </div>
             </div>
           </div>
         </div>
