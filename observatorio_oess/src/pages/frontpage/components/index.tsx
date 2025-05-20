725aa54c44585624002119ca5b502bf02b3253b5
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-05-19 22:02:47 -0600

    Icons Fixed

--- observatorio_oess/src/pages/frontpage/components/index.tsx
+++ observatorio_oess/src/pages/frontpage/components/index.tsx
@@ -12,6 +12,10 @@ import Foto2 from "../../../assets/Image/Carrousel/cruz roja.jpg";
 import Foto6 from "../../../assets/Image/Carrousel/fedeagua.jpg";
 import Foto1 from "../../../assets/Image/Carrousel/feria 3.jpg";
 import Foto5 from "../../../assets/Image/Carrousel/feria 4.jpg";
+import Icon1 from "../../../assets/Image/PropIcons/solidarity.png";
+import Icon2 from "../../../assets/Image/PropIcons/corporate.png";
+import Icon3 from "../../../assets/Image/PropIcons/united-nations.png";
+import Icon4 from "../../../assets/Image/PropIcons/goal.png";
 
 import "../style.css";
 
@@ -23,7 +27,7 @@ const items = [
   {
     id: 1,
     title: "¿Qué es Economía Social Solidaria?",
-    img: "https://img.icons8.com/external-kiranshastry-lineal-kiranshastry/64/external-heart-charity-kiranshastry-lineal-kiranshastry-1.png",
+    img: Icon1,
     desc:
       "Según la Asamblea General de las Naciones Unidas, la Economía Social Solidaria engloba a: empresas, organizaciones y otras entidades que realizan actividades tanto económicas como sociales o medioambientales bajo un interés colectivo como general, partiendo desde los principios de: la cooperación voluntaria y la ayuda mutua, la gobernanza democrática o participativa, la autonomía y la independencia, y la primacía de las personas y el fin social sobre el capital en la distribución y el uso de los excedentes o los beneficios, así como de los activos.\n\n" +
       "Además, se caracterizan por poner en práctica un conjunto de valores que son intrínsecos a su funcionamiento y acordes con el cuidado de las personas y el planeta, la igualdad y la equidad, la interdependencia, la auto gobernanza, la transparencia y la rendición de cuentas, y el logro del trabajo decente y de medios de vida dignos.",
@@ -31,7 +35,7 @@ const items = [
   {
     id: 2,
     title: "\nOrganizaciones de la Economía Social Solidaria en Costa Rica",
-    img: "https://img.icons8.com/ios/64/organization.png",
+    img: Icon2,
     desc:
       "Las organizaciones reconocidas como integrantes de la Economía Social Solidaria según el Decreto Ejecutivo N° 39835 MP-MTSS:\n\n" +
       "1. Asociaciones Cooperativas. \n" +
@@ -47,7 +51,7 @@ const items = [
   {
     id: 3,
     title: "\nDirección Nacional de Economía Social Solidaria",
-    img: "https://img.icons8.com/ios/64/museum.png",
+    img: Icon3,
     desc:
       "La Dirección Nacional de Economía Social Solidaria es una dependencia del Ministerio de Trabajo y Seguridad Social, creada mediante el Decreto Ejecutivo: 41059-MTSS. La Dirección de Economía Social Solidaria (DESS), tiene como objetivo impulsar y articular las distintas políticas públicas para fortalecer el sector privado de la Economía Social Solidaria que existe en Costa Rica, buscando generar trabajo decente, combatiendo a la pobreza y desigualdad.\n\n" +
       'En el marco internacional, la DESS reconoce la resolución de la Asamblea General de las Naciones Unidas: "Promover la economía social y solidaria para el desarrollo sostenible A/77/L.60". Esta resolución alienta a los Estados Miembros a implementar: estrategias políticas y programas a nivel nacional, que busquen fomentar la economía social y solidaria como un posible modelo de desarrollo socioeconómico sostenible, al igual que como un instrumento de planificación y programación, en particular el Marco de Cooperación de las Naciones Unidas para el Desarrollo Sostenible.',
@@ -55,7 +59,7 @@ const items = [
   {
     id: 4,
     title: "\nObservatorio ESS",
-    img: "https://img.icons8.com/material-outlined/64/planetarium.png",
+    img: Icon4,
     desc:
       "El objetivo del Observatorio de Economía Social Solidaria es: dotar al país con instrumentos que permitan gestionar de manera ideal la información de los sectores que integran la Economía Social Solidaria, facilitando su comprensión y la medición del impacto que generan en la economía.\n\n" +
       "Es por lo anterior, que desde la Dirección de Economía Social Solidaria del Ministerio de Trabajo y Seguridad Social (MTSS), con la colaboración de la Comisión Económica para América Latina (CEPAL), hemos convenido en la unificación de esfuerzos para presentar el primer Observatorio de la Economía Social Solidaria de Costa Rica.",
@@ -69,13 +73,15 @@ const Single = ({ item }: any) => {
     target: ref,
   });
 
-  const x = useTransform(scrollYProgress, [0.7, 1], [-190, 950]);
+  const x = useTransform(scrollYProgress, [0.7, 1], [2, 950]);
 
   return (
     <section ref={ref}>
       <div className="container">
         <div className="wrapper">
-          <img className="propInfo" src={item.img} alt="prop" />
+          <div className="propIcon">
+            <img src={item.img} alt="prop" />
+          </div>
           <motion.div className="textContainer" style={{ x }}>
             <h2 dangerouslySetInnerHTML={{ __html: replaceBr(item.title) }} />
             <p dangerouslySetInnerHTML={{ __html: replaceBr(item.desc) }} />
