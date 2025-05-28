b744d4b556be492e9e535c7f927f05fd3442f298
Ignacio Zelada <109553320+IgnacioZAraya@users.noreply.github.com>
2025-05-27 19:53:00 -0600

    New PBI Link

--- observatorio_oess/src/pages/reporte/components/index.tsx
+++ observatorio_oess/src/pages/reporte/components/index.tsx
@@ -8,7 +8,7 @@ function ReportPage() {
         <iframe
           title="sitio_vrs_11_variante3"
           className="info-Bi"
-          src="https://app.powerbi.com/view?r=eyJrIjoiM2JlNjBlYzItZjI4Mi00MzQ2LWI3NjQtZTdlMGExNGRhZDNiIiwidCI6IjM1ZDRkMWFhLWFlYTItNDI3Ny1iMGUyLWJkNDM3OWQ2OGVkMiJ9&pageName=d152ce7acdd17a756b95"
+          src="https://app.powerbi.com/view?r=eyJrIjoiYjQ5NWI4NWMtMGEwMy00NDEzLTg4MDItYzY1MmNkNzI5YjRkIiwidCI6IjM1ZDRkMWFhLWFlYTItNDI3Ny1iMGUyLWJkNDM3OWQ2OGVkMiJ9"
           frameBorder="0"
           allowFullScreen={true}
         ></iframe>
